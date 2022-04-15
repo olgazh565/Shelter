59462557e2fd9a1748fbb65223da293914b1cc29
olgazh565 <o_zh@mail.ru>
2022-04-15 23:11:50 +0300

    feat: add adaptive

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,4 +1,60 @@
-console.log('Hello world!');
+
+// Burger-menu --------------------------------------------
+
+const menu = document.querySelector('.nav_burger');
+const openMenuButton = document.querySelector('.nav__burger_open');
+const closeMenuButton = document.querySelector('.nav__burger_close');
+const blackout = document.querySelector('.header__blackout');
+console.log(openMenuButton)
+
+function openBurgerMenu(){
+    menu.classList.add('active');
+    blackout.classList.add('active');
+    openMenuButton.style.transform = 'rotate(90deg)';
+    closeMenuButton.style.transform = '';
+}
+
+function closeBurgerMenu(){
+    menu.classList.remove('active');
+    blackout.classList.remove('active');
+    closeMenuButton.style.transform = 'rotate(90deg)';
+    openMenuButton.style.transform = '';
+}
+
+openMenuButton.onclick = function() {
+    openBurgerMenu()
+}
+
+closeMenuButton.onclick = function() {
+    closeBurgerMenu()
+}
+
+const closeMenuBurger = document.querySelector('.nav__item_burger');
+
+closeMenuBurger.onclick = function(event) {
+    event.preventDefault();
+
+    closeBurgerMenu()
+}
+
+const blackoutParent = document.querySelector('.header__burger');
+
+blackoutParent.onclick = function(event) {
+    if (event.target === blackout){
+        closeBurgerMenu()
+    }   return
+}
+
+const disabledMenuLinks = document.querySelectorAll('a.disabled');
+
+for (let link of disabledMenuLinks) {
+    link.onclick = function(event) {
+        event.preventDefault()        
+    }
+}
+
+console.log(disabledMenuLinks)
+
 
 // const swiper = new Swiper('.swiper', {
 //     // Optional parameters
